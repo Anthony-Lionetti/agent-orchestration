131e41a5b09ca0cd0f4fed9926764940ba6079e7
Anthony Lionetti <lionetti.tech@gmail.com>
2025-07-26 15:51:19 -0400

    feat: add unified logging to rabbitmq logic

--- mq/__init__.py
+++ mq/__init__.py
@@ -1,5 +1,5 @@
 from .connection import MQHandler
-# from .publisher import MessagePublisher
+from .publisher import publish
 from .consumer import MessageConsumer
 
-__all__ = ["MQHandler", "MessageConsumer"]
+__all__ = ["MQHandler", "MessageConsumer", "publish"]
